4b4601b4566008e4a0cc4fff20077eb3a132b78b
the-racer-306 <khaledmamdouh306@gmail.com>
2023-03-09 20:54:38 +0200

    calcultor

---
+++ calculator.py
@@ -0,0 +1,15 @@
+input_a = input("First Number: ")
+
+operation = input("enter the operation: ")
+
+input_b = input("Second Number: ")
+
+
+if operation == "+":
+    print(int(input_a) + int(input_b) )
+elif operation == "-":
+    print(int(input_a) - int(input_b) )
+elif operation == "/":
+    print(int(input_a) / int(input_b) )
+elif operation == "*":
+    print(int(input_a) * int(input_b) )
